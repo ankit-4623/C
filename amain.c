db2da13a83b5c1f513ef9090fedc01ccfb9fc888
ankit4326 <you@example.com>
2024-04-14 00:42:17 +0530

    first commit

---
+++ amain.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main()
+{
+    printf("Can it push to  git");
+}
---
+++ ankit.c
@@ -0,0 +1,13 @@
+long long numberOfSubarrays(int* a, int n) {
+    long long count = 0;
+    for (int i = 0; i < n; i++) {
+        int maxele = a[i];
+        for (int j = i; j < n; j++) {
+            maxele = (a[j] > maxele) ? a[j] : maxele;
+            if (a[i] == maxele&&a[j]==maxele) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
